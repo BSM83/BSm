624524b471dd8a714563c8d0bc5fc540ae137630
BSM83 <muhamedyasb@gmail.com>
2026-07-01 10:43:46 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,59 @@
-const t=new Date('2026-08-08T12:00:00').getTime();
-setInterval(()=>{
-const d=t-Date.now();
-days.innerText=Math.max(0,Math.floor(d/86400000));
-hours.innerText=Math.max(0,Math.floor((d%86400000)/3600000));
-minutes.innerText=Math.max(0,Math.floor((d%3600000)/60000));
-seconds.innerText=Math.max(0,Math.floor((d%60000)/1000));
-},1000);
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbygFytYDoaNy4RvkH_cn38uZLT8Lw8BiTxNaSkb_UywtAqEtx74VbOP40iqgXGl0WhJMg/exec";
+
+const form = document.getElementById("rsvpForm");
+const successMessage = document.getElementById("successMessage");
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    submitForm();
+});
+
+async function submitForm() {
+
+    const fullname = document.getElementById("fullname").value.trim();
+
+    const status = document.querySelector(
+        'input[name="status"]:checked'
+    ).value;
+
+    const data = {
+        fullname: fullname,
+        status: status
+    };
+
+    try {
+
+        const response = await fetch(SCRIPT_URL, {
+            method: "POST",
+            body: JSON.stringify(data)
+        });
+
+        if (response.ok) {
+
+            successMessage.style.display = "block";
+
+            form.reset();
+
+            document.querySelector(
+                'input[value="Иә, міндетті түрде келемін"]'
+            ).checked = true;
+
+            successMessage.scrollIntoView({
+                behavior: "smooth"
+            });
+
+        } else {
+
+            alert("Қате пайда болды.");
+
+        }
+
+    } catch (err) {
+
+        console.log(err);
+
+        alert("Интернет байланысын тексеріңіз.");
+
+    }
+
+}
